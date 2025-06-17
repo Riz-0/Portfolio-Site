5ff7069eb50c68d7cfdfa0196cf821bb2b3e42d2
Riz <55377396+Riz-0@users.noreply.github.com>
2025-06-17 13:58:40 +0800

    Add date to status bar

--- app.js
+++ app.js
@@ -3,12 +3,38 @@ function main() {
 }
 
 function updateTime() {
+  let d = new Date();
+
+  // Set time
+  let hours = (d.getHours() < 10 ? "0" : "") + d.getHours();
+  let minutes = (d.getMinutes() < 10 ? "0" : "") + d.getMinutes();
   const timeEm = document.querySelector(".time");
-  let date = new Date();
-  let hours = (date.getHours() < 10 ? "0" : "") + date.getHours();
-  let minutes = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
-  let time = hours + ":" + minutes;
-  timeEm.textContent = time;
+  timeEm.textContent = hours + ":" + minutes;
+
+  // Set date
+  const weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const dateEm = document.querySelector(".date");
+  let day = d.getDay() - 1;
+  let date = d.getDate();
+  let month = d.getMonth() - 1;
+  let year = d.getFullYear();
+  dateEm.textContent = `${weekdays[day]}, ${date} ${months[month]} ${year}`;
+
+  // Update every 1000ms/1s
   setTimeout(updateTime, 1000);
 }
 
